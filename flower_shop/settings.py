dd3eae10a2a202ee01ca99c914f41921954455c7
ludwig300 <mr4silent@gmail.com>
2023-08-25 22:04:51 +0700

    Revert "Updated settings"
    This reverts commit eac02e3430e29f2f21c5995f454964d98f6688b4.

--- flower_shop/settings.py
+++ flower_shop/settings.py
@@ -12,7 +12,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', False)
 DJANGO_ENV = env('DJANGO_ENV', 'production')
-
+YANDEX_API_KEY = env('YANDEX_API_KEY')
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
