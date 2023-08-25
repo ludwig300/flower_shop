d8631e96effc73912c7d97143a52fd6247f4d7fd
ludwig300 <mr4silent@gmail.com>
2023-08-25 22:25:48 +0700

    Fixed bugs

--- flower_shop/settings.py
+++ flower_shop/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'flower_shop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
